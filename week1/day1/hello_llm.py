7a6d7f716d6fc7f796f4f1d54be91d21a62c62c0
Ayushkant3011 <ayushkant17@gmail.com>
2026-09-17 18:01:18 +0530

    First LLM Call

---
+++ week1/day1/hello_llm.py
@@ -0,0 +1,28 @@
+import os
+# from pathlib import path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API KEY NOT FOUND")
+
+client = Groq(api_key = my_api_key)
+
+model="openai/gpt-oss-20b"
+role="user"
+prompt="Do you know Ayush Kant"
+message={
+    "role": role,
+    "content":prompt
+}
+
+messages=[message]
+
+response=client.chat.completions.create(model=model, messages= messages)
+print(response)
+
+answer = response.choices[0].message.content
+print(answer)
